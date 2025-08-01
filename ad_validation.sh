d26133326d23e6033c140c08b2e7d832a9bc070e
AM1221MA <157356700+AM1221MA@users.noreply.github.com>
2025-07-31 22:14:27 -0700

    Update ad_validation.sh

--- ad_validation.sh
+++ ad_validation.sh
@@ -3,17 +3,17 @@ USERNAME=$1
 SAMACCOUNTNAME=$(echo "$USERNAME" | cut -d@ -f1)
 USER_HOME=$(getent passwd "$USERNAME" | cut -d: -f6)
 SSH_DIR="$USER_HOME/.ssh"
-AUTHORIZED_KEYS="$SSH_DIR/authorized_keys"
-LOG="/tmp/ssh_key_debug.log"
+AUTH_KEYS="$SSH_DIR/authorized_keys"
+LOGS="/tmp/ssh_key_debug.log"
 echo "$USERNAME"
 echo "$SAMMACOUNTNAME"
 echo "$SSH_DIR"
-echo "[$(date)] USERNAME=$USERNAME" >> "$LOG"
-echo "SAMACCOUNTNAME=$SAMACCOUNTNAME" >> "$LOG"
-echo "USER_HOME=$USER_HOME" >> "$LOG"
+echo "[$(date)] USERNAME=$USERNAME" >> "$LOGS"
+echo "SAMACCOUNTNAME=$SAMACCOUNTNAME" >> "$LOGS"
+echo "USER_HOME=$USER_HOME" >> "$LOGS"
 # Si ya tiene la llave guardada localmente, usar esa
-if [[ -f "$AUTHORIZED_KEYS" ]]; then
-    tail -n 1 "$AUTHORIZED_KEYS"
+if [[ -f "$AUTH_KEYS" ]]; then
+    tail -n 1 "$AUTH_KEYS"
     exit 0
 fi
 # Si no, buscarla en AD
@@ -24,20 +24,20 @@ BIND_PW=$(cat /etc/ssh/ldap_bind_pw)
 KEY=$(ldapsearch -x -o ldif-wrap=no \
     -H "$LDAP_URI" \
     -D "$BIND_DN" -w "$BIND_PW" \
-    -b "$BASE_DN" "(sAMAccountName=$SAMACCOUNTNAME)" altSecurityIdentities 2>>"$LOG" | \
+    -b "$BASE_DN" "(sAMAccountName=$SAMACCOUNTNAME)" altSecurityIdentities 2>>"$LOGS" | \
     grep '^altSecurityIdentities: SSHKey:' | sed 's/^altSecurityIdentities: SSHKey://')
-echo "$KEY" >> "$LOG"
+echo "$KEY" >> "$LOGS"
 # Crear .ssh y guardar la llave
 if [[ ! -d "$SSH_DIR" ]]; then
     mkdir -p "$SSH_DIR"
     chown "$USERNAME": "$SSH_DIR"
     chmod 700 "$SSH_DIR"
 fi
-echo "$KEY" > "$AUTHORIZED_KEYS"
-chown "$USERNAME": "$AUTHORIZED_KEYS"
-chmod 600 "$AUTHORIZED_KEYS"
+echo "$KEY" > "$AUTH_KEYS"
+chown "$USERNAME": "$AUTH_KEYS"
+chmod 600 "$AUTH_KEYS"
 chown "$USERNAME": "$USER_HOME"
 chmod 700 "$USER_HOME"
 # Mostrar la llave como salida
 echo "$KEY"
-chmod 700 "$LOG"
+chmod 700 "$LOGS"
